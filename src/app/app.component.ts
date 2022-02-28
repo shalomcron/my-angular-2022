d906c7e65e7192f716b63ced51693ebe36ab3612
shalom <shalomcron@gmail.com>
2022-02-28 23:07:09 +0200

    gg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'my-angular';
+
+  content = 'content11';
+  tooltip = 'tooltip11';
+
+
+   value: number = 6;
+   options: Options = {
+      showTicksValues: true,
+      floor: 0,
+      ceil: 36,
+
+      step: 1,
+      showTicks: true,
+      tickStep: 6
+   };
+
+
+//   value: number = 5;
+//   options: Options = {
+//     showTicksValues: true,
+//     stepsArray: [
+//       { value: 1, legend: "Very poor" },
+//       { value: 2 },
+//       { value: 3, legend: "Fair" },
+//       { value: 4 },
+//       { value: 5, legend: "Average" },
+//       { value: 6 },
+//       { value: 7, legend: "Good" },
+//       { value: 8 },
+//       { value: 9, legend: "Excellent" }
+//     ]
+//   };
+
 }
